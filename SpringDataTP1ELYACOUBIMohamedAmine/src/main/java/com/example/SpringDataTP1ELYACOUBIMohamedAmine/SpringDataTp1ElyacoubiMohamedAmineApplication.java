8fbcfc46215e61c223bad7ae7786f94ef0b321b0
Amine <elyyamine@gmail.com>
2025-11-06 23:23:58 +0100

    first commit

---
+++ SpringDataTP1ELYACOUBIMohamedAmine/src/main/java/com/example/SpringDataTP1ELYACOUBIMohamedAmine/SpringDataTp1ElyacoubiMohamedAmineApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataTP1ELYACOUBIMohamedAmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataTp1ElyacoubiMohamedAmineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataTp1ElyacoubiMohamedAmineApplication.class, args);
+	}
+
+}
---
+++ SpringDataTP1ELYACOUBIMohamedAmine/src/test/java/com/example/SpringDataTP1ELYACOUBIMohamedAmine/SpringDataTp1ElyacoubiMohamedAmineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataTP1ELYACOUBIMohamedAmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataTp1ElyacoubiMohamedAmineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
